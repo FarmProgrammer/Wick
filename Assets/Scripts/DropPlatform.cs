68bb7f8dc4071b823ca8e7b9a5ee4e9aa862f404
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2025-11-17 12:49:47 +0000

    Misc

---
+++ Assets/Scripts/DropPlatform.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DropPlatform : MonoBehaviour
+{
+    [SerializeField, Range(0f, 2f)]
+    private float dropDelay=0.3f;
+    private bool hasDropped = false;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.collider.tag);
+        if (col.collider.tag == "Player" && !hasDropped)
+        {
+            hasDropped = true;
+            Invoke("Drop", dropDelay);
+        }
+    }
+
+    private void Drop()
+    {
+        Destroy(this.gameObject);
+    }
+}
